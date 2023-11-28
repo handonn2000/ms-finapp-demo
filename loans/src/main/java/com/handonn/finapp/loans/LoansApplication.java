cace8d43aad1605d541113f6b8c133427bc9fa27
handonn2000 <handonn542000@gmail.com>
2023-11-28 23:49:20 +0700

    feat: dynamic configurations loading

--- loans/src/main/java/com/handonn/finapp/loans/LoansApplication.java
+++ loans/src/main/java/com/handonn/finapp/loans/LoansApplication.java
@@ -3,9 +3,11 @@ package com.handonn.finapp.loans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication(scanBasePackages = {"com.handonn.finapp.loans", "com.handonn.finapp.common"})
 @ConfigurationPropertiesScan(value = "com.handonn.finapp.loans.config")
+@RefreshScope
 public class LoansApplication {
 
 	public static void main(String[] args) {
