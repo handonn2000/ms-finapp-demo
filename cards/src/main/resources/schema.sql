f589ad800afd2e96143fcb32558876d0952a12a8
handonn2000 <handonn542000@gmail.com>
2023-12-23 00:41:48 +0700

    feat: set up postgres DB with docker compose

--- cards/src/main/resources/schema.sql
+++ cards/src/main/resources/schema.sql
@@ -1,16 +1,15 @@
-CREATE TABLE IF NOT EXISTS `cards` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `mobile_number` varchar(15) NOT NULL,
-  `card_number` varchar(100) NOT NULL,
-  `card_type` varchar(100) NOT NULL,
-  `total_limit` int NOT NULL,
-  `amount_used` int NOT NULL,
-  `available_amount` int NOT NULL,
-  `created_at` date NOT NULL,
-  `created_by` varchar(20) NOT NULL,
-  `modified_at` date DEFAULT NULL,
-  `modified_by` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS cards (
+  id serial PRIMARY KEY,
+  mobile_number varchar(15) NOT NULL,
+  card_number varchar(100) NOT NULL,
+  card_type varchar(100) NOT NULL,
+  total_limit int NOT NULL,
+  amount_used int NOT NULL,
+  available_amount int NOT NULL,
+  created_at date NOT NULL,
+  created_by varchar(20) NOT NULL,
+  modified_at date DEFAULT NULL,
+  modified_by varchar(20) DEFAULT NULL
 );
 
 INSERT INTO CARDS (mobile_number, card_number, card_type, total_limit, amount_used, available_amount, created_at, created_by)
